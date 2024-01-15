00dcd932a6f8eafe51c25494fc5ba524a8113ab9
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2024-01-15 09:47:03 +0100

    use homework 11

---
+++ src/main.js
@@ -0,0 +1,103 @@
+'use strict';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.form');
+const input = document.querySelector('#input');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = input.value.trim();
+  gallery.innerHTML = '';
+  input.value = '';
+  loader.style.display = 'block';
+
+  // object iterator
+  const searchParams = new URLSearchParams({
+    key: '41494285-2be0c6d487dc7750955372a82',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  fetch(`https://pixabay.com/api/?${searchParams}`)
+    .then(response => {
+      // loader.style.display = 'none';
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
+        return response.json();
+      } else {
+        throw new Error('Invalid server response');
+      }
+    })
+    .then(data => {
+      loader.style.display = 'none';
+
+      if (!data.hits || !data.hits.length) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          theme: 'dark',
+          backgroundColor: '#EF4040',
+          titleColor: 'white',
+          messageSize: '16',
+          titleSize: '16',
+          position: 'topRight',
+          maxWidth: 430,
+        });
+      } else {
+        const imgs = data.hits.reduce(
+          (
+            html,
+            {
+              webformatURL,
+              largeImageURL,
+              tags,
+              likes,
+              views,
+              comments,
+              downloads,
+            }
+          ) =>
+            html +
+            `<li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+           <img class="gallery-image"
+           src="${webformatURL}"
+           alt="${tags}"
+           />
+          </a>          
+          <div class="description">
+          <p>Likes:<span>${likes}</span></p>
+          <p>Views:<span>${views}</span></p>
+          <p>Comments:<span>${comments}</span></p>
+          <p>Downloads:<span>${downloads}</span></p>
+          </div> 
+        </li>`,
+          ''
+        );
+
+        gallery.innerHTML = imgs;
+
+        let modal = new simpleLightbox('ul.gallery a', {
+          captionDelay: 250,
+          captionsData: 'alt',
+        });
+
+        modal.refresh();
+      }
+    })
+    .catch(err => {
+      loader.style.display = 'none';
+      console.log(err);
+    });
+});
